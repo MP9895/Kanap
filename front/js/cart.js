a87d3203f2833b7f6245241f4dc023c1c0148b95
Martin <martinperoni95@gmail.com>
2023-02-09 23:14:28 +0100

    fonction pour supprimer un produit du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -17,46 +17,46 @@ else {
 
 // Affichage des produits dans la page panier (avec les prix en fetch)
 function display(products) {
-    products.forEach(product => {displayOne(product)})
+    products.forEach(product => { displayOne(product) })
 }
 function displayOne(product) {
     // AFFICHAGE DU/DES PRODUIT(S) PANIER
 
     // insertion des articles
-    var createArticle = document.createElement('article');
+    let createArticle = document.createElement('article');
     createArticle.className = 'cart__item';
     createArticle.setAttribute('data-id', product.id);
     createArticle.setAttribute('data-color', product.color);
     document.querySelector('#cart__items').appendChild(createArticle);
 
     // insertion div de l'img
-    var createDivIMG = document.createElement('div');
+    letcreateDivIMG = document.createElement('div');
     createDivIMG.className = 'cart__item__img';
     createArticle.appendChild(createDivIMG);
 
     // insertion des images
-    var createIMG = document.createElement('img');
+    letcreateIMG = document.createElement('img');
     createIMG.setAttribute('src', product.imageUrl);
     createIMG.setAttribute('alt', "Photographie d'un canapé");
     createDivIMG.appendChild(createIMG);
 
     // insertion div content description
-    var createDivContDes = document.createElement('div');
+    letcreateDivContDes = document.createElement('div');
     createDivContDes.className = 'cart__item__content';
     createArticle.appendChild(createDivContDes);
 
     // insertion div description
-    var createDivDes = document.createElement('div');
+    let createDivDes = document.createElement('div');
     createDivDes.className = 'cart__item__content__description';
     createDivContDes.appendChild(createDivDes);
 
     // insertion H2
-    var createH2 = document.createElement('h2');
+    let createH2 = document.createElement('h2');
     createH2.textContent = product.name;
     createDivDes.appendChild(createH2);
 
     // insertion P color
-    var createpColor = document.createElement('p');
+    let createpColor = document.createElement('p');
     createpColor.textContent = "Couleur : " + product.color;
     createDivDes.appendChild(createpColor);
 }
@@ -75,3 +75,25 @@ function buildCompleteList(all) {
     return list;
 }
 
+// Suppression d'un prduit dans le panier
+function deleteProduct() {
+    let deleteItem = document.querySelectorAll('.deleteItem');
+    for (let i = 0; i < delItem.length; i++) {
+        deleteItemUnit = deleteItem[i];
+        deleteItemUnit.addEventListener('click', function(event) {
+            let articleDeleteItemID = event.target.closest('article').getAttribute("data-id");
+            let articleDeleteItemColor = event.target.closest('article').getAttribute("data-color");
+            
+            var basket = JSON.parse(basketStr);   
+            productToDelete = basket.products.find(element => element.id == articleDeleteItemID && element.color == articleDeleteItemColor);
+            
+            result = basket.products.filter(element => element.id !== articleDeleteItemID || element.color !== articleDeleteItemColor);
+            basket.products = result;
+
+            newQuantity = basket.totalQuantity - productToDelete.quantity;
+            basket.totalQuantity = newQuantity;
+            priceToDelete = productToDelete.quantity * productToDelete.price;
+            alert('Vous avez bien supprimé votre produit du panier');
+        })
+    };
+}
